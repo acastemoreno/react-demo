830947ddb34251cf04b5cbfe059af75785b1b987
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-02-19 17:29:47 -0500

    Cambiado texto por defecto de InfoBox.

--- src/component/InfoBox.jsx
+++ src/component/InfoBox.jsx
@@ -1,24 +1,25 @@
 "use strict";
 
 var InfoBox = React.createClass({
+    getDefaultProps: function(){
+        return{
+            data: {
+                text: "",
+                value: "",
+            }
+        };
+    },
     render: function(){
         var data = this.props.data;
-        
-        if (data)
-            return(
-                <div id="infoBox">
-                    <ol>
-                        <li><label>Etiqueta:</label> <span>{data.text}</span></li>
-                        <li><label>Cantidad:</label> <span>{data.value}</span></li>
-                    </ol>
-                </div>
-            );
-        else
-            return(
-                <div id="infoBox">
-                    <p>Seleccione una etiqueta</p>
-                </div>
-            );
+        return(
+            <div id="infoBox">
+                <ol>
+                    <li><label>Etiqueta:</label> <span>{data.text}</span></li>
+                    <li><label>Cantidad:</label> <span>{data.value}</span></li>
+                </ol>
+            </div>
+        );
+
     }
 });
 
