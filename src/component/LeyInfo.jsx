d879244264f6b59e75ca1020e1cc34e0a815fafc
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-02-20 00:27:28 -0500

    Informacion de cada ley

--- src/component/LeyInfo.jsx
+++ src/component/LeyInfo.jsx
@@ -2,10 +2,17 @@
 
 var LeyInfo = React.createClass({
     render: function(){
-        var ley = this.props.data;
+        var ley = this.props;
+        var lower = ley.nombre.toLowerCase();
+        var nombre = lower.charAt(0).toUpperCase() + lower.slice(1);
         return(
             <div>
-                <label>Nombre</label> <span></span>
+                <p><label>Nombre:</label>
+                    <span>{nombre}</span></p>
+                <p><label>Legislatura:</label>
+                    <span>{ley.legislatura}</span></p>
+                <p><label>autografa:</label>
+                    <span><a href={ley.pdf} target="_blank">link</a></span></p>
             </div>
         );
     }
