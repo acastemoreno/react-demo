8602c7b0e23cef59ac2e7aa9580779baf3528c9d
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-02-25 04:32:50 -0500

    Eventos del teclado en el tagBrowser.

--- src/component/TagListItem.jsx
+++ src/component/TagListItem.jsx
@@ -4,10 +4,19 @@ var TagListItem = React.createClass({
     onClickHandler: function(event){
         this.props.onClickCallback(this.props.data);
     },
+    onKeyPressHandler: function(event){
+        if(event.key == "Enter"){
+            this.props.onClickCallback(this.props.data);
+            event.stopPropagation();
+        }
+    },
     render: function(){
         return(
-            <li onClick={this.onClickHandler}>
-                {this.props.data.text}
+            <li onClick={this.onClickHandler} >
+                <span tabIndex={2}
+                      onKeyPress={this.onKeyPressHandler}>
+                    {this.props.data.text}
+                </span>
             </li>
         );
     }
