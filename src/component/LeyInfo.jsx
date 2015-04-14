1a928565094558a7c7caf1a80eb6f6f1b5c030e3
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-04-13 21:37:58 -0500

    Agregada fecha a LeyInfo

--- src/component/LeyInfo.jsx
+++ src/component/LeyInfo.jsx
@@ -1,19 +1,35 @@
 "use strict";
 
+var ReactDate = React.createClass({
+    render: function(){
+        var fecha = this.props.date.split("/");
+        var date = new Date(fecha.reverse());
+        var listaFecha = [date.getUTCDate(),
+                          date.getUTCMonth(),
+                          date.getUTCFullYear()];
+        return(
+            <time dateTime={date.toString()}>{this.props.date}</time>
+        );
+    }
+});
+
 var LeyInfo = React.createClass({
     render: function(){
         var ley = this.props;
         var lower = ley.nombre.toLowerCase();
         var nombre = lower.charAt(0).toUpperCase() + lower.slice(1);
+        var fecha = this.props["fecha promulga"];
         return(
-            <div>
-                <p><label>Nombre:</label>
-                    <span>{nombre}</span></p>
-                <p><label>Legislatura:</label>
-                    <span>{ley.legislatura}</span></p>
-                <p><label>autografa:</label>
-                    <span><a href={ley.pdf} target="_blank">link</a></span></p>
-            </div>
+            <dl className="leyInfo">
+                <dt>Nombre</dt>
+                <dd>{nombre}</dd>
+                <dt>Fecha</dt>
+                <dd><ReactDate date={fecha}/></dd>
+                <dt>Legislatura</dt>
+                <dd>{ley.legislatura}</dd>
+                <dt>autografa</dt>
+                <dd><a href={ley.pdf} target="_blank">link</a></dd>
+            </dl>
         );
     }
 });
